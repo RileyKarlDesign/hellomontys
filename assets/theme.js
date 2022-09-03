35e11648bbea4b7907c3c0775aa1090424461925
riley karl <rileykarldesign@gmail.com>
2022-09-03 17:04:37 +1200

    main flows layed out

---
+++ assets/theme.js
@@ -0,0 +1,45 @@
+const header = document.querySelector('.header');
+const headerHeight = document.querySelector('.header').clientHeight;
+const sectionOne= document.querySelector('.spacer');
+
+const sectionOneOptions = { rootMargin:  "-10% 0px 0px 0px"};
+
+
+const sectionOneObserver = new IntersectionObserver(function(entries, sectionOneObserver){
+
+    entries.forEach( entry => {
+
+        if(!entry.isIntersecting){
+            header.classList.add('header-scrolled')
+            console.log('class added')
+        }else{
+            header.classList.remove('header-scrolled') 
+        }
+     
+
+
+
+    })
+}, sectionOneOptions )
+
+sectionOneObserver.observe( sectionOne )
+
+
+
+function onReady(callback) {
+    var intervalId = window.setInterval(function() {
+      if (document.getElementsByTagName('body')[0] !== undefined) {
+        window.clearInterval(intervalId);
+        callback.call(this);
+      }
+    }, 1000);
+  }
+  
+  function setVisible(selector, visible) {
+    document.querySelector('#content').classList.remove('.content-load');
+  }
+  
+  onReady(function() {
+    setVisible('.page', true);
+    setVisible('#loading', false);
+  });
