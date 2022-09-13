c85e594ffaffa58c6fce4d81f198338b6399aea1
riley karl <rileykarldesign@gmail.com>
2022-09-13 22:11:52 +1200

    filter form and product page updates

--- assets/theme.js
+++ assets/theme.js
@@ -22,3 +22,23 @@ enterView({
 
 
 
+//  filters ----------------
+
+const filterForm = document.querySelector('.filter-form')
+const filterLabel = document.querySelector('.filter-label')
+
+filterLabel.addEventListener('click', () => {
+	
+if(filterForm.classList.contains('open') ){
+	
+	filterForm.classList.remove('open')
+	filterLabel.innerHTML=" Filters +"
+} else{
+	filterForm.classList.add('open')
+	filterLabel.innerHTML=" Filters -"
+}
+	
+})
+
+
+
