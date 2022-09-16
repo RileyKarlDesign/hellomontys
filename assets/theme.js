3c1ecd74d29debceccd5245727799518c307d69b
riley karl <rileykarldesign@gmail.com>
2022-09-16 16:29:34 +1200

    filters complete

--- assets/theme.js
+++ assets/theme.js
@@ -1,5 +1,8 @@
 
-console.log('heelop')
+console.log('Hello, Welcome. You might be here to find holes ion this site and before you look ill tell you , there are many. Your just a crusty dude who cannot make meaningfull relations with out the concept of money or fear playing a part. You are a intitled peice of shit that reaks of generational wealth and i hoope you come to the understand that its not other people who are the problem it is you. It is clear you have no idea how to run a company and have little to no respect for thoes who work for you. Burning people into the ground in order to fifill your horrible time lines is insane. I hope your company fails and your employees move on to do great things without you. ')
+
+
+
 const foot = document.querySelector('.footer')
 const nav = document.querySelector('.nav-wrapper')
 
